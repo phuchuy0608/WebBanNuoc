8147cb6ffa3e5ecce0a595827d7f901ae576d00d
phuchuy0608 <huykhungz123@gmail.com>
2022-05-14 19:13:03 +0700

    ngay 14

--- WebBanNuoc/Controllers/HomeController.cs
+++ WebBanNuoc/Controllers/HomeController.cs
@@ -16,6 +16,16 @@ namespace WebBanNuoc.Controllers
             HomeIndexViewModel model = new HomeIndexViewModel();
             return View(model.CreateModel(search,4,page));
         }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        public ActionResult CheckoutDetails()
+        {
+            return View();
+        }
         public ActionResult AddToCart(int productId, string url)
         {
             if (Session["cart"] == null)
@@ -65,6 +75,41 @@ namespace WebBanNuoc.Controllers
             return Redirect(url);
         }
 
+        public ActionResult DecreaseQty(int productId, string url)
+        {
+            if (Session["cart"] != null)
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var count = cart.Count();
+                var product = ctx.Tbl_Product.Find(productId);
+                for (int i = 0; i < count; i++)
+                {
+                    if (cart[i].Product.ProductId == productId)
+                    {
+                        if (cart[i].Quantity > 1)
+                        {
+                            int prevQty = cart[i].Quantity;
+                            cart.Remove(cart[i]);
+                            cart.Add(new Item()
+                            {
+                                Product = product,
+                                Quantity = prevQty - 1
+                            });
+                            break;
+                        }
+                        else
+                        {
+                            cart.Remove(cart[i]);
+                            break;
+                        }
+                    }
+                    
+                }
+                Session["cart"] = cart;
+            }
+            return Redirect(url);
+        }
+        
         public int SumProduct()
         {
             int s = 0;
@@ -79,16 +124,15 @@ namespace WebBanNuoc.Controllers
         public ActionResult RemoveFromCart(int productId)
         {
             List<Item> cart = (List<Item>)Session["cart"];
-            foreach (var item in cart)
+            Item item = cart.SingleOrDefault(n => n.Product.ProductId == productId);
+            if(item != null)
             {
-                if (item.Product.ProductId == productId)
-                {
-                    cart.Remove(item);
-                    break;
-                }
+                cart.RemoveAll(n => n.Product.ProductId == productId);
+                 return RedirectToAction("Index");
             }
-            Session["cart"] = cart;
-            return Redirect("Index");
+            cart = null;
+            return RedirectToAction("Index");
+
         }
     }
 }
